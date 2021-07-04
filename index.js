f27653b27b7ff75949d35c94ec970ac4ed7d4f4e
M3hran <31813529+m3hransh@users.noreply.github.com>
2021-07-04 22:34:01 +0430

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ client.on("message", (message) => {
     message.channel.send("Pong.");
   } else if (message.content === `${prefix}beep`) {
     message.channel.send("Boop!!");
-  }else if (message.content === `${prefix}bing`) {
-    message.channel.send("PONG!!");
+  }else if (message.content === `${prefix}cat`) {
+    message.channel.send("cat back!!");
   } else if (message.content === `${prefix}server`) {
       message.channel.send(`this server's name is: ${message.guild.name}`)
   } else if (message.content === `${prefix}hi`) {
