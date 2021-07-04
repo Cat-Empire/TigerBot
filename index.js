54d6cbd001cf9a2c87b7ce142da395f6c1eb9bff
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-07-04 21:35:58 +0430

    Change index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on("message", (message) => {
   if (message.content === `${prefix}ping`) {
     message.channel.send("Pong.");
   } else if (message.content === `${prefix}beep`) {
-    message.channel.send("Boop.");
+    message.channel.send("Boop!!");
   } else if (message.content === `${prefix}server`) {
       message.channel.send(`this server's name is: ${message.guild.name}`)
   } else if (message.content === `${prefix}hi`) {
