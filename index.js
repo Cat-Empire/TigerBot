669caa501c1aed2a22127ba6c0a6c26b5180b2d8
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-12-10 08:34:19 +0330

    remove undwanted files

--- index.js
+++ index.js
@@ -14,19 +14,22 @@ data = {};
 client.on("message", (message) => {
   if (message.content === `${prefix}ping`) {
     if (data[message.author.username] !== undefined)
-      data[message.author.username] += 1
-    else
-      data[message.author.username] = 1;
-    
-    message.channel.send(`You ${message.author.username} issued ping for ${data[message.author.username]} times.`);
+      data[message.author.username] += 1;
+    else data[message.author.username] = 1;
+
+    message.channel.send(
+      `You ${message.author.username} issued ping for ${
+        data[message.author.username]
+      } times.`
+    );
   } else if (message.content === `${prefix}beep`) {
     message.channel.send("Boop!!");
-  }else if (message.content === `${prefix}cat`) {
+  } else if (message.content === `${prefix}cat`) {
     message.channel.send("cat back!!");
   } else if (message.content === `${prefix}server`) {
-      message.channel.send(`this server's name is: ${message.guild.name}`)
+    message.channel.send(`this server's name is: ${message.guild.name}`);
   } else if (message.content === `${prefix}hi`) {
-      message.channel.send(`Hello dear ${message.author.username}`)
+    message.channel.send(`Hello dear ${message.author.username}`);
   }
 });
 
